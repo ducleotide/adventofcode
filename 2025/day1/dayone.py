ba2d1d3c5097b40736b00c8ac0610b2a58cae584
Duc N Nguyen <duc@ducleotide.com>
2025-12-02 14:29:41 -0500

    day one 2025 part one and two

--- 2025/day1/dayone.py
+++ 2025/day1/dayone.py
@@ -1,9 +1,17 @@
+import math
+from enum import Enum
+
 import aoc_common
 from pydantic import BaseModel
 
 
+class Direction(Enum):
+    LEFT = 'L'
+    RIGHT = 'R'
+
+
 class Rotation(BaseModel):
-    direction: str
+    direction: Direction
     amount: int
     # def __init__(self, dir: str, rot: int):
     #     self.direction = dir
@@ -14,33 +22,41 @@ def load_rotations(filename):
     with open(filename, 'r') as f:
         rotations: list[Rotation] = list[Rotation]()
         for l in f:
-            dial = Rotation(direction=l[0], amount=int(l[1:]))
+            dial = Rotation(direction=Direction(l[0]), amount=int(l[1:]))
             rotations.append(dial)
 
         return rotations
 
-def perform_rotation(current_position: int, rotation: Rotation, max_dial=99):
-    resulting_position: int = -999
 
-    if rotation.direction == "L":
-        new_pos = current_position - (rotation.amount % (max_dial + 1))
+def perform_rotation(current_position: int, rotation: Rotation, max_dial=100):
+    resulting_position: int = -999
+    clicks: int = 0
+
+    clicks += math.trunc(rotation.amount / max_dial)
+    if rotation.direction == Direction.LEFT:
+        new_pos = current_position - (rotation.amount % max_dial)
         if new_pos < 0:
-            resulting_position = (max_dial + 1) + new_pos
+            resulting_position = max_dial + new_pos
+            if current_position != 0:
+                clicks += 1
         else:
             resulting_position = new_pos
-    elif rotation.direction == "R":
-        new_pos = current_position + (rotation.amount % (max_dial + 1))
-        if new_pos > (max_dial + 1):
-            resulting_position = new_pos - (max_dial + 1)
+    elif rotation.direction == Direction.RIGHT:
+        new_pos = current_position + (rotation.amount % max_dial)
+        if new_pos > max_dial:
+            resulting_position = new_pos - max_dial
+            if current_position != 0:
+                clicks += 1
         else:
             resulting_position = new_pos
     else:
         raise Exception(f"Unknown direction: {rotation.direction} or amount {rotation.amount}")
 
-    if resulting_position == (max_dial + 1):
+    if resulting_position == max_dial or resulting_position == 0 :
         resulting_position = 0
+        clicks += 1
 
-    return resulting_position
+    return resulting_position, clicks
 
 
 def main():
@@ -50,13 +66,18 @@ def main():
 
     res_pos = 50
     ct = 0
+    zeroes = 0
     print(f"The dial starts at {res_pos}")
     for rotation in rotations:
-        res_pos = perform_rotation(res_pos, rotation)
+        res_pos, clicks = perform_rotation(res_pos, rotation)
+        ct += clicks
         if res_pos == 0:
-            ct += 1
-        print(f"The dial is rotated {rotation.direction}{rotation.amount} to point {res_pos}")
-    print(f"Number of time pointing at 0: {ct}")
+            zeroes += 1
+        print(f"The dial is rotated {rotation.direction.value}{rotation.amount} to point {res_pos}. "
+              f"It points at 0 {clicks} times")
+    print(f"Number of clicks {ct}")
+    print(f"Number of time pointing at 0: {zeroes}")
+
 
 if __name__ == "__main__":
     main()
