d34118a955e74fd66c7d7db59a5c86f5da3e285d
Duc N Nguyen <duc@ducleotide.com>
2025-12-02 12:39:40 -0500

    2025 aoc day1 part 1

--- 2025/day1/dayone.py
+++ 2025/day1/dayone.py
@@ -1,2 +1,62 @@
 import aoc_common
+from pydantic import BaseModel
 
+
+class Rotation(BaseModel):
+    direction: str
+    amount: int
+    # def __init__(self, dir: str, rot: int):
+    #     self.direction = dir
+    #     self.rotation = rot
+
+
+def load_rotations(filename):
+    with open(filename, 'r') as f:
+        rotations: list[Rotation] = list[Rotation]()
+        for l in f:
+            dial = Rotation(direction=l[0], amount=int(l[1:]))
+            rotations.append(dial)
+
+        return rotations
+
+def perform_rotation(current_position: int, rotation: Rotation, max_dial=99):
+    resulting_position: int = -999
+
+    if rotation.direction == "L":
+        new_pos = current_position - (rotation.amount % (max_dial + 1))
+        if new_pos < 0:
+            resulting_position = (max_dial + 1) + new_pos
+        else:
+            resulting_position = new_pos
+    elif rotation.direction == "R":
+        new_pos = current_position + (rotation.amount % (max_dial + 1))
+        if new_pos > (max_dial + 1):
+            resulting_position = new_pos - (max_dial + 1)
+        else:
+            resulting_position = new_pos
+    else:
+        raise Exception(f"Unknown direction: {rotation.direction} or amount {rotation.amount}")
+
+    if resulting_position == (max_dial + 1):
+        resulting_position = 0
+
+    return resulting_position
+
+
+def main():
+    args = aoc_common.aoc_parse_args()
+
+    rotations = load_rotations(args.inputfile)
+
+    res_pos = 50
+    ct = 0
+    print(f"The dial starts at {res_pos}")
+    for rotation in rotations:
+        res_pos = perform_rotation(res_pos, rotation)
+        if res_pos == 0:
+            ct += 1
+        print(f"The dial is rotated {rotation.direction}{rotation.amount} to point {res_pos}")
+    print(f"Number of time pointing at 0: {ct}")
+
+if __name__ == "__main__":
+    main()
---
+++ 2025/day1/test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class DayOneTestCase(unittest.TestCase):
+    def test_perform_rotation_left(self):
+
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
