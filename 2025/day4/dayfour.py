69cef3eba04145ffed1245cf967a3f15af5a8833
Duc N Nguyen <duc@ducleotide.com>
2025-12-04 10:59:13 -0500

    day4 part2

--- 2025/day4/dayfour.py
+++ 2025/day4/dayfour.py
@@ -42,21 +42,44 @@ def count_rolls(input_table: list[str], x_x:int, x_y:int) -> int:
     return ret
 
 
-
-def print_table(input_table: list[str], x_x:int, x_y:int) -> None:
+def print_table(input_table: list[str], x_x:int = -1, x_y:int = -1) -> None:
     for y in range(len(input_table)):
-        if x_y == y:
+        if x_y != -1 and x_y == y:
             before = input_table[y][:x_x]
             after = input_table[y][x_x+1:]
             line = before + 'X' + after
         else:
             line = input_table[y]
-
         print(line)
+
+
+def remove_roll(input_table, x_x, x_y) -> list[str]:
+    updated_table: list[str] = []
+    for y in range(len(input_table)):
+        if y == x_y:
+            before = input_table[y][:x_x]
+            after = input_table[y][x_x + 1:]
+            line = before + '.' + after
+        else:
+            line = input_table[y]
+        updated_table.append(line)
+    # print_table(updated_table)
+    return updated_table
+
+
+def remove_rolls(input_table: list[str], rolls_to_remove: list[(int,int)]):
+    updated_table: list[str]= input_table
+
+    for x, y in rolls_to_remove:
+        # print(f"removing ({x},{y})")
+        updated_table = remove_roll(updated_table, x, y)
+
+    return updated_table
 
 
 def count_rolls_in_table(input_table, func=count_rolls):
     roll_count = 0
+    rolls_to_remove: list[(int,int)] = []
     for y in range(len(input_table)):
         for x in range(len(input_table[y])):
             # print_table(input_table, x, y)
@@ -65,18 +88,41 @@ def count_rolls_in_table(input_table, func=count_rolls):
 
                 if num_rolls <= 4:
                     roll_count += 1
-                print(f"num rolls {num_rolls} -- rollcount -- {roll_count}")
-            print("------")
-    return roll_count
+                    rolls_to_remove.append((x,y))
+                # print(f"num rolls {num_rolls} -- rollcount -- {roll_count}")
+            # print("------")
+    # print(f"rolls to remove: {rolls_to_remove}")
+    updated_table = remove_rolls(input_table, rolls_to_remove)
+    return roll_count, updated_table
+
+
+def count_and_remove_rolls(input_table) -> int:
+    tot_rolls_removed = 0
+    num_rolls_removed = 999
+
+    curr_table = input_table
+    # print_table(curr_table)
+    ct = 0
+    while num_rolls_removed > 0:
+        num_rolls_removed, curr_table = count_rolls_in_table(curr_table)
+        # print_table(curr_table)
+        print(f"iteration: {ct} num rolls removed {num_rolls_removed}")
+        tot_rolls_removed += num_rolls_removed
+        ct += 1
+    return tot_rolls_removed
 
 
 def main():
     args = aoc_common.aoc_parse_args()
     input_table = aoc_common.load_input_table(args.inputfile)
 
-    count_part1 = count_rolls_in_table(input_table)
+    count_part1, _ = count_rolls_in_table(input_table)
     print(f"Part 1: {count_part1}")
+    print("============")
 
+    input_table = aoc_common.load_input_table(args.inputfile)
+    count_part2 = count_and_remove_rolls(input_table)
+    print(f"Part 2: {count_part2}")
 
 if __name__ == "__main__":
     main()
