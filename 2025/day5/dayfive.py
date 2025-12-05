c52fdce4397fe515f81856fcc300756d6d01c55f
Duc N Nguyen <duc@ducleotide.com>
2025-12-05 10:44:07 -0500

    day5 part2

--- 2025/day5/dayfive.py
+++ 2025/day5/dayfive.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import aoc_common
 from pydantic import BaseModel
 
@@ -7,7 +9,7 @@ class IngredientRange(BaseModel):
     end_id: int
 
 
-def load_input_file(filename: str):
+def load_input_file(filename: str) -> Tuple[list[IngredientRange], list[int]]:
     ingredient_ranges: list[IngredientRange] = []
     ingredient_ids: list[int] = []
 
@@ -26,13 +28,45 @@ def load_input_file(filename: str):
 
 
 def is_fresh(id: int, ingredient_ranges: list[IngredientRange]) -> bool:
-    ret = False
-
     for ingredient_range in ingredient_ranges:
         if ingredient_range.start_id <= id <= ingredient_range.end_id:
             return True
+    return False
 
-    return ret
+
+def create_fresh_ids(ingredient_ranges: list[IngredientRange]) -> list[IngredientRange]:
+    union_ranges: list[IngredientRange] = []
+
+    # sort by starting_id
+    sorted_ranges = sorted(ingredient_ranges, key=lambda x: x.start_id)
+
+    for ingredient_range in sorted_ranges:
+        # print(f"ingredient range{ingredient_range}")
+        if len(union_ranges) == 0:
+            union_ranges.append(ingredient_range)
+            continue
+
+        if ingredient_range.start_id <= union_ranges[-1].end_id:
+            # union_ranges[-1].end_id = ingredient_range.end_id
+            if ingredient_range.end_id > union_ranges[-1].end_id:
+                union_ranges[-1].end_id = ingredient_range.end_id
+            else:
+                # do nothing
+                pass
+        else:
+            union_ranges.append(ingredient_range)
+
+    return union_ranges
+
+
+def count_union_ranges(union_ranges: list[IngredientRange]) -> int:
+    count = 0
+    for ingredient_range in union_ranges:
+        num_fresh = ingredient_range.end_id - ingredient_range.start_id + 1
+        print(f"range: {ingredient_range}: num_fresh: {num_fresh}")
+        count += num_fresh
+
+    return count
 
 
 def main():
@@ -46,6 +80,11 @@ def main():
             fresh_list.append(id)
 
     print(f"part 1: num fresh {len(fresh_list)}")
+
+    union_ranges = create_fresh_ids(ingredient_ranges)
+    # print(f"union ranges: {union_ranges}")
+    num_fresh_ids = count_union_ranges(union_ranges)
+    print(f"part2: num fresh_ids {num_fresh_ids}")
 
 if __name__ == "__main__":
     main()
