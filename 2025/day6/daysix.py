69160e38a539abc43af85384af33cb3b7f499be2
Duc N Nguyen <duc@ducleotide.com>
2025-12-08 06:45:53 -0500

    wip day6 part2

--- 2025/day6/daysix.py
+++ 2025/day6/daysix.py
@@ -44,6 +44,47 @@ def calculate_table(table_t: list[list[int]], operations: list[str]) -> list[int
     return ret
 
 
+def load_input_table_part2(inputfile: str) -> Tuple[list[list[str]], list[str]]:
+    with open(inputfile, 'r') as f:
+        input_table: list[list[str]] = []
+        operations: list[str] = []
+        for line in f:
+            if line[0] == '+' or line[0] == '*':
+                operations = line.strip().split()
+            else:
+                num_nums = len(line.split())
+
+                row: list[str] = [" " for _ in range(num_nums)]
+                for i in range(len(line)):
+                    row[i] = line[i]
+                input_table.append(row)
+
+    return input_table, operations
+
+
+def transpose_part2(input_table: list[list[str]]):
+    """Transposes a 2D array represented as a list of strings."""
+    if not input_table:
+        return []
+
+    # Convert the list of strings to a 2D NumPy array of characters
+    char_arr = np.array([list(row) for row in input_table])
+
+    # Transpose the array and join characters back into strings
+    return [list(row) for row in char_arr.T]
+
+
+def calculate_table_part2(table_t: list[list[int]], operations: list[str]) -> list[int]:
+    ret: list[int] = []
+    i = 0
+    for row in table_t:
+        oper = operations[i]
+        # create new 2d array for each row
+
+
+    return ret
+
+
 def main():
     args = aoc_common.aoc_parse_args()
 
@@ -55,6 +96,9 @@ def main():
     answers = calculate_table(table_t, operations)
     print(f"answers\n{answers}")
     print(f"PART 1: {sum(answers)}")
+
+    answers_part2 = calculate_table_part2(table_t, operations)
+    print(f"answers part 2: {answers_part2}")
     
 
 if __name__ == "__main__":
